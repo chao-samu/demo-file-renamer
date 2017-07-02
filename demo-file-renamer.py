9977b519d2ae30b1c761c45948d2e534201a2c88
chao-samu <chao-samu@protonmail.com>
2017-07-02 14:37:15 +0200

    added more renaming options, some GUI fixes

--- demo-file-renamer.py
+++ demo-file-renamer.py
@@ -3,15 +3,15 @@
 
 #  =================================================================================================
 #  Python Version ......: 3.*
-#  Version .............: 1.10 (Python)
-#  Release Date ........: 2017-06-30
+#  Version .............: 1.11 (Python)
+#  Release Date ........: 2017-07-02
 #  GitHub ..............: https://github.com/chao-samu/demo-file-renamer
 #  Author ..............: chao-samu
 # --------------------------------------------------------------------------------------------------
 #  Script Name..........: demo file renamer
 #  Description .........: Integrate mapname into demofile (*.dem).
 #                         Support: TF2, CS 1.6, CS:S and CS:GO
-#  License..............: MIT License 
+#  License..............: MIT License
 #                         (https://github.com/chao-samu/demo-file-renamer/blob/master/LICENSE)
 #  =================================================================================================
 
@@ -114,17 +114,18 @@ class OnErrorEvent(wx.PyCommandEvent):
 # GUI ==============================================================================================
 class MainWindow(wx.Frame):
     def __init__(self, parent, title):
-        wx.Frame.__init__(self, parent, title=title, size=(345,320), style=wx.DEFAULT_FRAME_STYLE ^ wx.RESIZE_BORDER)
+        wx.Frame.__init__(self, parent, title=title, size=(345,330), style=wx.DEFAULT_FRAME_STYLE ^ wx.RESIZE_BORDER)
 
         # programm variables
         self.PrgName = title
-        self.PrgVersion = "v1.05"
+        self.PrgVersion = "v1.11"
         self.PytonVersion = sys.version
         self.WxVersion = wx.__version__
         IconPath = "_ico" + os.sep + "Demofile Renamer.ico"
 
         # init variables
         self.ListSelection = 0
+        self.CheckBoxSelection = False
         textExample = 'Example:\n "samurai-vs-ninja.dem" will be renamed to\n "samurai-vs-ninja_de_dust2.dem"'
 
         # Center frame
@@ -141,9 +142,9 @@ class MainWindow(wx.Frame):
         helpmenu = wx.Menu()
 
         # MenuBar colums
-        menuHelp = helpmenu.Append(wx.ID_HELP)
+        menuHelp = helpmenu.Append(wx.ID_HELP, '&Help', 'Show help dialog')
         menuSeperator = helpmenu.AppendSeparator()
-        menuLicense = helpmenu.Append(wx.ID_VIEW_DETAILS, "License") # should use wx.newid()
+        menuLicense = helpmenu.Append(wx.ID_VIEW_DETAILS, "&License", "Show license dialog") # should use wx.newid()
         menuAbout = helpmenu.Append(wx.ID_ABOUT)
         menuSeperator = helpmenu.AppendSeparator()
         menuExit = helpmenu.Append(wx.ID_EXIT)
@@ -156,15 +157,18 @@ class MainWindow(wx.Frame):
         # FramePanel creation
         panel = wx.Panel(self, -1)
         wx.StaticText(panel, -1, "Choose renaming mask:",(30,15))
-        
+
         self.ListBoxSelect = wx.ListBox(panel, -1, choices=["OLDNAME_MAPNAME", "MAPNAME_OLDNAME", \
-        "YYYY-MM-DD_MAPNAME_OLDNAME", "YYYY-MM-DD_OLDNAME_MAPNAME"],size=(270,35),pos=(30,40))
-        
+        "YYYY_MAPNAME_OLDNAME", "YYYY_OLDNAME_MAPNAME", "YYYY-MM_MAPNAME_OLDNAME", \
+        "YYYY-MM_OLDNAME_MAPNAME", "YYYY-MM-DD_MAPNAME_OLDNAME", "YYYY-MM-DD_OLDNAME_MAPNAME"], \
+        size=(270,35),pos=(30,40))
+
         self.StaticTextExample = wx.StaticText(panel, -1, textExample,(30,80))
         wx.StaticText(panel, -1, "Extracting mapname",(30,150))
         self.GaugeProgress = wx.Gauge(panel, -1, range=100, pos=(30,170),size=(270,10))
-        self.buttonCancel = wx.Button(panel, -1, "Cancel", (30,200),(90,20))
-        self.buttonStart = wx.Button(panel, -1, "Start", (210,200),(90,20))
+        self.checkboxMode = wx.CheckBox(panel, -1, "Parsing full demofile (processing intense)",  pos=(30,190), size=(270,15))
+        self.buttonCancel = wx.Button(panel, -1, "Cancel", (30,218),(90,20))
+        self.buttonStart = wx.Button(panel, -1, "Start", (210,218),(90,20))
 
         # Preselect default settings
         self.ListBoxSelect.SetSelection(0)
@@ -175,6 +179,7 @@ class MainWindow(wx.Frame):
         self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
         self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
         self.Bind(wx.EVT_LISTBOX, self.OnListSelect, self.ListBoxSelect)
+        self.Bind(wx.EVT_CHECKBOX, self.OnCheckBoxSelect, self.checkboxMode)
         self.Bind(wx.EVT_BUTTON, self.OnStart, self.buttonStart)
         self.Bind(wx.EVT_BUTTON, self.OnCancel, self.buttonCancel)
 
@@ -208,6 +213,12 @@ class MainWindow(wx.Frame):
             are not included).
             In a protected folder (like "ProgramFiles") you have to start this program in admin mode!
         3. Press OK and wait until you will hopefully be happy :)
+
+        Options Info:
+        Parsing full demofile: This option is recommended for old demofiles (CS 1.6 etc.),
+        because it was able to record into the same file when a mapchange occur. So the hole
+        file have to be read to detect all played maps in one file. This was fixed in newer
+        games (CS:GO etc.), so it isn't necessary there. Use this option if you are unsure.
         """
         MessageDlgText = textwrap.dedent(MessageDlgText)
 
@@ -222,7 +233,7 @@ class MainWindow(wx.Frame):
 
         The MIT License (MIT)
 
-        Copyright (c) 2015 chao-samu
+        Copyright (c) 2017 chao-samu
 
         Permission is hereby granted, free of charge, to any person obtaining a copy
         of this software and associated documentation files (the "Software"), to deal
@@ -249,7 +260,7 @@ class MainWindow(wx.Frame):
         dlg.Destroy() # finally destroy it when finished.
 
     def OnAbout(self, event):
-        # MessageDialog creation      
+        # MessageDialog creation
         MessageDlgText = self.PrgName + ":\n" + self.PrgVersion + "\n\nBased on Python version:\n" + \
         self.PytonVersion + "\n\nGUI made with wxPython version:\n" + self.WxVersion + "\n\nMade by chao-samu" + \
         "\nLook for updates: https://github.com/chao-samu"
@@ -270,11 +281,26 @@ class MainWindow(wx.Frame):
             textExample = 'Example:\n "samurai-vs-ninja.dem" will be renamed to\n "de_dust2_samurai-vs-ninja.dem"'
             self.StaticTextExample.SetLabel(textExample)
         elif self.ListSelection == 2:
+            textExample = 'Example:\n "samurai-vs-ninja.dem" will be renamed to\n "2007_de_dust2_samurai-vs-ninja.dem"'
+            self.StaticTextExample.SetLabel(textExample)
+        elif self.ListSelection == 3:
+            textExample = 'Example:\n "samurai-vs-ninja.dem" will be renamed to\n "2007_samurai-vs-ninja_de_dust2.dem"'
+            self.StaticTextExample.SetLabel(textExample)
+        elif self.ListSelection == 4:
+            textExample = 'Example:\n "samurai-vs-ninja.dem" will be renamed to\n "2007-05_de_dust2_samurai-vs-ninja.dem"'
+            self.StaticTextExample.SetLabel(textExample)
+        elif self.ListSelection == 5:
+            textExample = 'Example:\n "samurai-vs-ninja.dem" will be renamed to\n "2007-05_samurai-vs-ninja_de_dust2.dem"'
+            self.StaticTextExample.SetLabel(textExample)
+        elif self.ListSelection == 6:
             textExample = 'Example:\n "samurai-vs-ninja.dem" will be renamed to\n "2007-05-03_de_dust2_samurai-vs-ninja.dem"'
             self.StaticTextExample.SetLabel(textExample)
         else:
             textExample = 'Example:\n "samurai-vs-ninja.dem" will be renamed to\n "2007-05-03_samurai-vs-ninja_de_dust2.dem"'
             self.StaticTextExample.SetLabel(textExample)
+
+    def OnCheckBoxSelect(self, event):
+        self.CheckBoxSelection = event.IsChecked()
 
     def OnStart(self, event):
         saveDirDialog = wx.DirDialog(self, "Please select your folder where your demofiles are.\n" \
@@ -286,7 +312,7 @@ class MainWindow(wx.Frame):
             StartSelect = self.StartRenaming()
             if StartSelect is True:
                 # start thread
-                worker = DemoFileRenamer(self, self.ListSelection)
+                worker = DemoFileRenamer(self, self.ListSelection, self.CheckBoxSelection)
                 worker.start()
         else:
             return     # the user changed idea...
@@ -307,7 +333,7 @@ class MainWindow(wx.Frame):
     def OnCancel(self, event):
         self.Close()
 
-    # THREAD EVENTS FOR "DemoFileRenamer" ---------------------------------------------------------
+    # EVENTS FOR "DemoFileRenamer" THREAD  ---------------------------------------------------------
     def OnGuiEnable(self, evt):
         self.buttonStart.Enable()
         self.buttonCancel.Enable()
@@ -352,9 +378,9 @@ class MainWindow(wx.Frame):
         dlg.ShowModal() # Show it
         dlg.Destroy() # finally destroy it when finished.
 
-# MAIN FUNCTION THREADED ===========================================================================
+# MAIN FUNCTION (THREADED) =========================================================================
 class DemoFileRenamer(threading.Thread):
-    def __init__(self, parent, ListSelection):
+    def __init__(self, parent, ListSelection, CheckBoxSelection):
         """
         @param parent: The gui object that should recieve the value
         @param value: value to 'calculate' to
@@ -362,17 +388,27 @@ class DemoFileRenamer(threading.Thread):
         threading.Thread.__init__(self)
         self._parent = parent
         self.ListSelection = ListSelection
+        self.CheckBoxSelection = CheckBoxSelection
 
     def run(self):
         """Overrides Thread.run. Don't call this directly its called internally
         when you call Thread.start().
         """
+
+        # INIT VARIABLES ---------------------------------------------------------------------------
+        mapname = ""
+        all_mapnames = []
         file_failed = ""
-        x = 0
-        file_failed_counter = 0
+
+        count_index = 0 # for all_mapnames
+        count_gauge_prg = 0
+        count_file_failed = 0
+
+        # LOOKING FOR DEM FILES --------------------------------------------------------------------
         demofiles = glob.glob('*' + os.extsep + 'dem')
         demofileCount = len(demofiles)
 
+        # PREPARE GUI ------------------------------------------------------------------------------
         evt = GaugeSetRangeEvent(myEVT_GAUGESETRANGE, -1, demofileCount)
         wx.PostEvent(self._parent, evt)
 
@@ -380,23 +416,64 @@ class DemoFileRenamer(threading.Thread):
         wx.PostEvent(self._parent, evt)
 
         print ("Files found: " + str(demofileCount) + "\nStarting renaming..." + "\nIn progress please wait...")
+
+        # START: MAIN ------------------------------------------------------------------------------
         for file_source in demofiles:
+            # START: OPEN FILE AND ITERATE IT LINE BY LINE (SEARCH MAPNAME) ------------------------
             with open(file_source, 'r', errors='ignore') as fobj:
                 for line in fobj:
                     mapname = re.search(r"(?<=maps[\/\\]).+(?=\.bsp)", line)
                     if mapname is not None:
-                        break
+                        mapname = mapname.group()
+                        if '/' or '\\' in mapname:
+                            mapname = re.sub(r".+[\/\\]", '', mapname)
+                            mapname = ''.join([x for x in mapname if x in string.printable])
+                        if self.CheckBoxSelection is True:
+                            if count_index == 0:
+                                all_mapnames.append(mapname)
+                                count_index += 1
+                            else:
+                                if mapname == all_mapnames[count_index-1]:
+                                    continue
+                                else:
+                                   all_mapnames.append(mapname)
+                                   count_index += 1
+                        else:
+                            break
+            # END: OPEN FILE AND ITERATE IT LINE BY LINE (SEARCH MAPNAME) --------------------------
+
+            # START: HANDLE RESULT (PREPARE VAR, RENAME FILE, EXCEPTION HANDLING) ------------------
+            if self.CheckBoxSelection is True:
+                mapname = '+'.join(all_mapnames)
+                del all_mapnames[:]
+                count_index = 0
+
             if mapname is not None:
-                mapname = mapname.group()
-                if '/' or '\\' in mapname:
-                    mapname = re.sub(r".+[\/\\]", '', mapname)
-                mapname = ''.join([x for x in mapname if x in string.printable])
+                if all_mapnames is not None:
+                    for n in all_mapnames:
+                        mapname = mapname + n
                 demoname, ext = os.path.splitext(file_source)
                 if self.ListSelection == 0:
                     file_destination = demoname + "_" + mapname + ext
                 elif self.ListSelection == 1:
                     file_destination = mapname + "_" + demoname + ext
                 elif self.ListSelection == 2:
+                    dt = os.path.getmtime(file_source)
+                    dt = datetime.datetime.fromtimestamp(dt).strftime('%Y')
+                    file_destination = dt + "_" + mapname + "_" + demoname + ext
+                elif self.ListSelection == 3:
+                    dt = os.path.getmtime(file_source)
+                    dt = datetime.datetime.fromtimestamp(dt).strftime('%Y')
+                    file_destination = dt + "_" + demoname + "_" + mapname + ext
+                elif self.ListSelection == 4:
+                    dt = os.path.getmtime(file_source)
+                    dt = datetime.datetime.fromtimestamp(dt).strftime('%Y-%m')
+                    file_destination = dt + "_" + mapname + "_" + demoname + ext
+                elif self.ListSelection == 5:
+                    dt = os.path.getmtime(file_source)
+                    dt = datetime.datetime.fromtimestamp(dt).strftime('%Y-%m')
+                    file_destination = dt + "_" + demoname + "_" + mapname + ext
+                elif self.ListSelection == 6:
                     dt = os.path.getmtime(file_source)
                     dt = datetime.datetime.fromtimestamp(dt).strftime('%Y-%m-%d')
                     file_destination = dt + "_" + mapname + "_" + demoname + ext
@@ -418,12 +495,15 @@ class DemoFileRenamer(threading.Thread):
                     return
             else:
                 file_failed = file_failed + file_source + '\n'
-                file_failed_counter += 1
-            x += 1
+                file_failed_count += 1
+            count_gauge_prg += 1
 
-            evt = GaugeProgressEvent(myEVT_GAUGEPROGRESS, -1, x)
+            evt = GaugeProgressEvent(myEVT_GAUGEPROGRESS, -1, count_gauge_prg)
             wx.PostEvent(self._parent, evt)
+            # END: HANDLE RESULT (PREPARE VAR, RENAME FILE, EXCEPTION HANDLING) --------------------
+        # END: MAIN --------------------------------------------------------------------------------
 
+        # START: FINISH (RESULT MESSAGES, LOG, GUI-UNLOCK) -----------------------------------------
         if file_failed:
             logfile_name = 'logfile_demo-file-renamer'
             logfile_ext = os.extsep + 'txt'
@@ -439,13 +519,13 @@ class DemoFileRenamer(threading.Thread):
             else:
                 logfile = open(logfile_name + logfile_ext, 'x').write(logfile_text)
 
-            evt = FinishFailedEvent(myEVT_FINISHFAILED, -1, logfile_name, logfile_ext, file_failed_counter)
+            evt = FinishFailedEvent(myEVT_FINISHFAILED, -1, logfile_name, logfile_ext, countfile_failed)
             wx.PostEvent(self._parent, evt)
 
-            print("Files failed to rename: " + str(file_failed_counter) + " of " + str(demofileCount) + \
+            print("Files failed to rename: " + str(count_file_failed) + " of " + str(demofileCount) + \
             "\nLogfile is generated, please look for further information in logfile: \"" + os.getcwd() + \
             os.sep + logfile_name + logfile_ext + "\"")
-            
+
         else:
             evt = FinishEvent(myEVT_FINISH, -1)
             wx.PostEvent(self._parent, evt)
@@ -453,8 +533,9 @@ class DemoFileRenamer(threading.Thread):
 
         evt = GUIEnableEvent(myEVT_GUIENABLE, -1)
         wx.PostEvent(self._parent, evt)
+        # START: FINISH (RESULT MESSAGES, LOG, GUI-UNLOCK) -----------------------------------------
 
 # wxPython window loop
 app = wx.App(False)
-frame = MainWindow(None, "Demo file renamer")
+frame = MainWindow(None, "demo file renamer")
 app.MainLoop()
